ce12f37ae8c95814a2507ba7d33ddea5d303a4f9
KAVATKAR <kavatkar-vishal.prabhakar@capgemini.com>
2022-02-23 23:28:36 +0530

    LCUC2

--- LineComparison.java
+++ LineComparison.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class LineComparison {
 	//Method to compute length of line
-		static void calculateLengthLine(double x1,double x2,double y1,double y2) 
+		static double calculateLengthLine(double x1,double x2,double y1,double y2) 
 		{
 	 	 double length_of_line=Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
-		 System.out.println("Length of the line is  :"+length_of_line);
+		 return length_of_line;
+		 
 	 	 
-		}
+	  }
 		
 	  public static void main(String[] args) {
 		 
-		   //Taking inputs from user
+		   //Taking inputs from system
 			Scanner scanner= new Scanner(System.in);
 		    System.out.println("enter points of first line and second line");
 		       
@@ -22,8 +23,26 @@ public class LineComparison {
 		       double x2=scanner.nextDouble();
 		       double y2=scanner.nextDouble();
 		       
+		     System.out.println("enter points of second line and second line");
+		     
+		       double p1=scanner.nextDouble();
+		       double q1=scanner.nextDouble();
+		       double p2=scanner.nextDouble();
+		       double q2=scanner.nextDouble();
+		       
 		       //calling the method
-		       calculateLengthLine(x1,x2,y1,y2);
+		       double length1 = calculateLengthLine(x1,x2,y1,y2);
+		       double length2 = calculateLengthLine(p1,p2,q1,q2);
+		       
+		       //check equality
+		       if(length1 == length2) 
+		       {
+		    	   System.out.println("Two line are equal");
+		       }
+		       else 
+		       {
+		    	   System.out.println("Two line are not equal");
+		       }
 		       
 		    scanner.close();
 		       
