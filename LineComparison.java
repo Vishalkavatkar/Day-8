8d9eb21a7dec5cba3d002ff6ed443e9c8ab921f7
KAVATKAR <kavatkar-vishal.prabhakar@capgemini.com>
2022-02-23 23:45:17 +0530

    LCUC4

--- LineComparison.java
+++ LineComparison.java
@@ -3,12 +3,29 @@ package lineComparison;
 import java.util.Scanner;
 
 public class LineComparison {
+	//method
 	static double calculateLengthLine(double x1,double x2,double y1,double y2) {
 	 	 double length_of_line=Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
 		 return length_of_line;
 		 
 	 	 
 	  }
+		
+		//equal and compareTo method
+		public void checkEquality(String s1,String s2) {
+			if(s1.equals(s2)) 
+			{
+		    	System.out.println("Two line are equal");
+		    }
+		    else if(s1.compareTo(s2)>0)
+		    {
+	     	    System.out.println("length of line1 is greater than length of line2");
+		    }
+		    else 
+		    {
+		    	System.out.println("length of line2 is greater than length of line1");
+		    }
+		}
 		
 	  public static void main(String[] args) {
 		 
@@ -32,18 +49,15 @@ public class LineComparison {
 		       double length1 = calculateLengthLine(x1,x2,y1,y2);
 		       double length2 = calculateLengthLine(p1,p2,q1,q2);
 		       
-		       //check equality
-		       if(length1 == length2) {
-		    	   System.out.println("Two line are equal");
-		       }
-		       else if(length1>length2){
-	        	   System.out.println("length of line1 is greater than lenth of line2");
-	           }
-		       else {
-		    	   System.out.println("length of line2 is greater than length of line1");
-		       }
+		       //converting double to string
+		       String s1=String.valueOf(length1);
+		       String s2=String.valueOf(length2);
 		       
-		    scanner.close();
+		       LineComparison compare =new LineComparison();
+		       compare.checkEquality(s1, s2);
+		       
+		            
+		       scanner.close();
 		       
 	 }
 
