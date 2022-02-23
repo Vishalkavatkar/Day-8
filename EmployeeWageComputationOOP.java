ab00bb0fcf21b7ce056c9cfc0957bd88126e5db8
KAVATKAR <kavatkar-vishal.prabhakar@capgemini.com>
2022-02-23 20:25:47 +0530

    Dailywage

--- EmployeeWageComputationOOP.java
+++ EmployeeWageComputationOOP.java
@@ -3,14 +3,25 @@ package employeeWageComputationOOP;
 public class EmployeeWageComputationOOP {
 
 	public static void main(String[] args) {
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		
+		int empHrs= 0;
+		int empWage = 0;
 		double attendance = Math.floor((Math.random() * 10) % 2);
 
-		if (attendance == 1) {
+		if (attendance == IS_FULL_TIME) {
 
-			System.out.println(" Employee is present. ");
-		} else {
+			System.out.println(" Employee is present. "); 
+			empHrs=8;
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage: " + empWage);
+		} 
+		else 
+		{
 			System.out.println(" Employee is absent. ");
 		}
+		
 
 	}
 
