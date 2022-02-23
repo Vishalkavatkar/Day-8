629a2be6e8c1c709722a67e4ddc726a995a2b398
KAVATKAR <kavatkar-vishal.prabhakar@capgemini.com>
2022-02-23 22:07:22 +0530

    monthly wage

--- EmployeeWageComputationOOP.java
+++ EmployeeWageComputationOOP.java
@@ -3,21 +3,29 @@ package employeeWageComputationOOP;
 public class EmployeeWageComputationOOP {
 
 	public static void main(String[] args) {
-		int attendance = (int) Math.floor((Math.random() * 3));
 		int wages = 20;
-		switch (attendance) {
+		int Monthly_Working_Days = 20;
+		int totalhrs = 0;
+		
+		for(int i=0; i<=20; i++) {
+			int attendance = (int) Math.floor((Math.random() * 3));
+			switch (attendance) {
 		case 0:
 			int empHrs = 4;
-			System.out.println("Employee is Present and worked Part Time: \n Employee Wage is " + empHrs * wages);
+			System.out.println("Employee is Present and worked Part Time");
+			totalhrs += empHrs;
 			break;
 		case 1:
 			int empHr = 8;
-			System.out.println(" Employee is Present and worked Full Time: \n Employee Wage is " + empHr * wages);
+			System.out.println(" Employee is Present and worked Full Time");
+			totalhrs += empHr;
 			break;
 		case 2:
 			System.out.println(" Employee is absent \n Wage is 0 ");
 			break;
 		}
+		}
+		System.out.println("The Monthly Wage is : " + totalhrs*wages);
 		
 	}
 
