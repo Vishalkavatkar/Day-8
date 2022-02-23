b3bfc8129e952d397f862a8abb0d02488ac18b5e
KAVATKAR <kavatkar-vishal.prabhakar@capgemini.com>
2022-02-23 21:04:00 +0530

    switchstatement

--- EmployeeWageComputationOOP.java
+++ EmployeeWageComputationOOP.java
@@ -3,31 +3,22 @@ package employeeWageComputationOOP;
 public class EmployeeWageComputationOOP {
 
 	public static void main(String[] args) {
-		//constants
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
-		//variable
-		int empHrs= 0;
-		int empWage = 0;
-		double attendance = Math.floor((Math.random() * 10) % 3);
-
-		if (attendance == IS_FULL_TIME) {
-
-			System.out.println("Employee is present and worked full time. "); 
-			empHrs=8;
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Emp Wage: " + empWage);
-		} 
-		else if(attendance == IS_PART_TIME)
-		{
-			System.out.println("Employee is present and worked half time. ");
-				empHrs = 4;
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Emp Wage: " + empWage);
+		int attendance = (int) Math.floor((Math.random() * 3));
+		int wages = 20;
+		switch (attendance) {
+		case 0:
+			int empHrs = 4;
+			System.out.println("Employee is Present and worked Part Time: \n Employee Wage is " + empHrs * wages);
+			break;
+		case 1:
+			int empHr = 8;
+			System.out.println(" Employee is Present and worked Full Time: \n Employee Wage is " + empHr * wages);
+			break;
+		case 2:
+			System.out.println(" Employee is absent \n Wage is 0 ");
+			break;
 		}
 		
-
 	}
 
 }
